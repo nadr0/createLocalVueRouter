f972898c55d5b0463fa886a752838ad62813c2ca
Kevin Nadro <khomer8@yahoo.com>
2020-04-25 19:34:20 -0500

    changing export statement.

--- source/index.js
+++ source/index.js
@@ -1,11 +1,7 @@
-const createLocalVueRouter = () => {
+export const createLocalVueRouter = () => {
   delete require.cache[require.resolve('vue-router')]; 
   if (window && window.location && window.location.href) {
     window.location.href = '/'
   }
   return require('vue-router').default
-}
-
-export default {
-  createLocalVueRouter
 }
